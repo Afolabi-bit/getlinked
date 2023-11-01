6ce6286ddf5fbd2d7ff635417ee24c8f73e5e0d6
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-11-01 07:16:45 +0100

    hero

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,11 @@
-import { Line } from "./utils";
+import { Line, Shadow } from "./utils";
 import Curve from "../assets/purple_curve.svg";
+import Chain from "../assets/chain.svg";
+import Fire from "../assets/fire.svg";
+
 const Hero = () => {
   return (
-    <section className="text-white">
+    <section className="text-white min-h-screen">
       <Line />
       <article className="container">
         <div className="mt-6 md:mt-8">
@@ -14,6 +17,26 @@ const Hero = () => {
             src={Curve}
             alt="#"
           />
+        </div>
+
+        <div className="relative text-center mt-6">
+          <Shadow left={"100px"} top={"50px"} />
+          <div className=" flex flex-col justify-center items-center">
+            <h1 className="z-10">
+              <span className="text-[32px] font-bold">getlinkedTech</span>
+            </h1>
+            <h1 className="z-50 flex items-center">
+              <span className="text-[32px] font-bold">
+                Hackathon<span className="text-[#D434FE]">1.0</span>
+              </span>
+              <img className="chain" src={Chain} alt="" />
+              <img className="fire" src={Fire} alt="" />
+            </h1>
+            <p className="text-[13px] font-normal center">
+              Participate in getlinked tech Hackathon 2023 stand a chance to win
+              a Big prize
+            </p>
+          </div>
         </div>
       </article>
     </section>
--- src/components/utils.jsx
+++ src/components/utils.jsx
@@ -1,3 +1,6 @@
+import Star from "../assets/star.svg";
+import BrightStar from "../assets/bright-star.svg";
+
 export const RegisterBtn = () => {
   return (
     <button className="register w-[172px] md:w-[122px] lg:w-[172px] text-white">
@@ -8,4 +11,13 @@ export const RegisterBtn = () => {
 
 export const Line = () => {
   return <div className="w-screen h-[1px] bg-[#ffffff2e]"></div>;
+};
+
+export const Shadow = ({ left, top }) => {
+  return (
+    <div
+      className="bg-shadow absolute z-[0] w-[1px] h-[1px]"
+      style={{ left: `${left}`, top: `${top}` }}
+    ></div>
+  );
 };
