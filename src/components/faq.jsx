a30af2b6ad00346c248d86fd97901dc26176e16a
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-11-04 13:54:13 +0100

    faq image

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -1,4 +1,9 @@
 import { Line, QuestionAnswer } from "./utils";
+import StarW from "../assets/star-white.svg";
+import StarD from "../assets/star-dim.svg";
+import StarP from "../assets/star-purple.svg";
+import StarG from "../assets/star-gra.svg";
+import Image from "../assets/faq.png";
 
 const Questions = [
   {
@@ -30,8 +35,13 @@ const Questions = [
 const FAQ = () => {
   return (
     <section>
-      <article className="my-[70px] flex flex-col items-center">
-        <div className="text-center px-8  md:px-[3rem] lg:w-[50%] lg:text-left">
+      <article className="my-[70px]  flex flex-col items-center">
+        <div className="relative mb-[70px] text-center px-8  md:px-[3rem] lg:w-[50%] lg:text-left lg:mb-0">
+          <img
+            src={StarP}
+            alt="#"
+            className="absolute scale-[1] top-[-30px] left-[14%] twinkle-fast"
+          />
           <h2 className="mb-3 font-bold">
             <span className="text-white text-[20px] leading-[133%] md:text-[32px]">
               Frequently Asked
@@ -51,6 +61,24 @@ const FAQ = () => {
               return <QuestionAnswer key={ind} obj={question} />;
             })}
           </div>
+        </div>
+        <div className="relative  w-full min-h-[400px] md:min-h-[550px] lg:min-h-[650px] lg:w-[50%] lg:mb-0">
+          <div className="absolute top-[18px] left-[70px]  flex gap-[49px] ">
+            <p className="question-mark text-[40px] font-medium text-[#A866FD] leading-[28px] ">
+              ?
+            </p>
+            <p className="question-mark translate-y-[-30px] text-[64px] text-[#D434FE] font-medium leading-[28px] ">
+              ?
+            </p>
+            <p className="question-mark text-[40px] font-medium text-[#A866FD] leading-[28px] ">
+              ?
+            </p>
+          </div>
+          <img
+            src={Image}
+            alt="#"
+            className="absolute bottom-[0%] left-[50%] translate-x-[-50%]  w-full  z-10 md:w-[80%] lg:w-[100%]"
+          />
         </div>
       </article>
       <Line />
