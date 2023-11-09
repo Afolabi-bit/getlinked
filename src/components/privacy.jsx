20a17d4c378734007185ccb430d9c37471f0c202
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-11-09 17:44:53 +0100

    Policies

--- src/components/privacy.jsx
+++ src/components/privacy.jsx
@@ -19,7 +19,7 @@ const PrivacyPolicy = () => {
           </p>
 
           {/* bordered box */}
-          <div className="py-[50px] px-[20px] border border-[#d434fe]  rounded-[5px] mt-[30px]">
+          <div className="py-[50px] flex flex-col items-center  px-[20px] border border-[#d434fe]  rounded-[5px] mt-[30px]">
             <p className="text-[12px]  text-center font-normal leading-[26px] mb-[30px]">
               At getlinked tech Hackathon 1.0, we value your privacy and are
               committed to protecting your personal information. This Privacy
@@ -28,46 +28,60 @@ const PrivacyPolicy = () => {
               participating in our event, you consent to the practices described
               in this policy.
             </p>
-            <h3 className="text-[#d434fe] text-[13px] font-bold leading-[28px]">
-              Licensing Policy
-            </h3>
-            <p className="text-[12px] font-bold leading-[26px]">
-              Here are terms of our Standard License:
-            </p>
-            <div className="flex mt-7">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                height="17"
-                viewBox="0 0 17 17"
-                fill="none"
-                className="w-[27px] h-[27px]"
-              >
-                <circle cx="8.5" cy="8.5" r="8.5" fill="#2DE100" />
-                <path d="M5 8L7 10.5L13.5 6" stroke="white" stroke-width="2" />
-              </svg>
-              <p className="text-[12px] ml-[10px] font-normal leading-[21px]">
-                The Standard License grants you a non-exclusive right to
-                navigate and register for our event
+            <div>
+              <h3 className="text-[#d434fe] text-[13px] font-bold leading-[28px]">
+                Licensing Policy
+              </h3>
+              <p className="text-[12px] font-bold leading-[26px]">
+                Here are terms of our Standard License:
               </p>
+              <div className="flex mt-7">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  className="w-[27px] h-[27px]"
+                >
+                  <circle cx="8.5" cy="8.5" r="8.5" fill="#2DE100" />
+                  <path
+                    d="M5 8L7 10.5L13.5 6"
+                    stroke="white"
+                    stroke-width="2"
+                  />
+                </svg>
+                <p className="text-[12px] ml-[10px] font-normal leading-[21px]">
+                  The Standard License grants you a non-exclusive right to
+                  navigate and register for our event
+                </p>
+              </div>
+              <div className="flex mt-7">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  className="w-[27px] h-[27px]"
+                >
+                  <circle cx="8.5" cy="8.5" r="8.5" fill="#2DE100" />
+                  <path
+                    d="M5 8L7 10.5L13.5 6"
+                    stroke="white"
+                    stroke-width="2"
+                  />
+                </svg>
+                <p className="text-[12px] ml-[10px] font-normal leading-[21px]">
+                  You are licensed to use the item available at any free source
+                  sites, for your project developement
+                </p>
+              </div>
             </div>
-            <div className="flex mt-7">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                height="17"
-                viewBox="0 0 17 17"
-                fill="none"
-                className="w-[27px] h-[27px]"
-              >
-                <circle cx="8.5" cy="8.5" r="8.5" fill="#2DE100" />
-                <path d="M5 8L7 10.5L13.5 6" stroke="white" stroke-width="2" />
-              </svg>
-              <p className="text-[12px] ml-[10px] font-normal leading-[21px]">
-                You are licensed to use the item available at any free source
-                sites, for your project developement
-              </p>
-            </div>
+
+            <button className="btn mt-[40px] mx-auto  px-[25px]">
+              Read More
+            </button>
           </div>
         </div>
       </article>
