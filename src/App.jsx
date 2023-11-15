67c5e20a21854e9052c41b558bf063621f05da18
Afolabi Joseph <maverickoluwatomisin@gmail.com>
2023-11-15 04:18:21 +0100

    routed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 
 const App = () => {
-  <Router>
-    <Routes>
-      <Route exact path="/" element={<Home />}></Route>
-    </Routes>
-  </Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
